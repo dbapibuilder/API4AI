a26b32d0a1e38acef90e9b6e99ca5bf9e390dce6
Martin <martin.li@makeiteasy.cn>
2024-10-03 05:25:18 +0800

    initial API4AI  commit

---
+++ function definition/README.md
@@ -0,0 +1,15 @@
+# Function Definition
+
+Copy content of each json file to openai/azure assistant functions
+
+<img  src="https://github.com/dbapibuilder/API4AI/blob/main/images/azure%20openai%20assistant%20functions.png">
+
+[Assistants Playground](https://platform.openai.com/playground/assistants).
+[Assistants API Overview (Python SDK)](https://cookbook.openai.com/examples/assistants_api_overview_python).
+
+| Functions   | Description| API       |
+|---------|-----|------------|
+| get_weather   | 1 | [New York](https://en.wikipedia.org/wiki/New_York_City)   |
+| search_internet     | 25  | Los Angeles |
+| send_email | 35  | Chicago    |
+| generate_image | 35  | Chicago    |
---
+++ tools.py
@@ -0,0 +1,82 @@
+import json
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOOLS_API_GATEWAY_URL = os.getenv("TOOLS_API_GATEWAY_URL")
+TOOLS_API_GATEWAY_KEY = os.getenv("TOOLS_API_GATEWAY_KEY")
+
+
+def get_weather(location: str):
+    
+    reqUrl = TOOLS_API_GATEWAY_URL + "/v1/weather"
+
+    headersList = {
+    "Content-Type": "application/json",
+    "Authorization": TOOLS_API_GATEWAY_KEY
+    }
+
+    payload = json.dumps({"location":location})
+
+    response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
+
+    return json.dumps(response.json())
+
+
+
+def search_internet(query: str):
+    reqUrl = TOOLS_API_GATEWAY_URL + "/v1/internet"
+
+    headersList = {
+    "Content-Type": "application/json",
+    "Authorization": TOOLS_API_GATEWAY_KEY 
+    }
+
+    payload = json.dumps({"query":query})
+
+    response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
+
+    #return json.dumps(response.json())
+    return response.text
+
+
+def send_email(to: str, subject: str, content: str):
+    reqUrl = TOOLS_API_GATEWAY_URL + "/v1/email"
+
+    headersList = {
+    "Content-Type": "application/json",
+    "Authorization": TOOLS_API_GATEWAY_KEY 
+    }
+
+    payload = json.dumps({"to":to, "subject":subject, "content":content})
+
+    response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
+
+    #return json.dumps(response.json())
+    return response.text
+
+def generate_image(prompt: str):
+    reqUrl = TOOLS_API_GATEWAY_URL + "/v1/image"
+
+    headersList = {
+    "Content-Type": "application/json",
+    "Authorization": TOOLS_API_GATEWAY_KEY 
+    }
+
+    payload = json.dumps({"prompt":prompt})
+
+    response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
+
+    #return json.dumps(response.json())
+    return response.text
+
+
+# Map the tool names to the functions on openai assistant control console
+TOOL_MAP = {"get_weather": get_weather,
+            "search_internet": search_internet,
+            "send_email": send_email,
+            "generate_image": generate_image
+
+            }
